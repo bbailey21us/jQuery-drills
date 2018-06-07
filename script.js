2d8d65a00a87b1c021ac0b9d98813792957badb9
bbailey21us <brandonbailey21@gmail.com>
2018-06-07 16:45:46 -0400

    troubleshooting

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
 
 $(document).ready(function(){
-    $("#btnSubmit").click(function(){
+    $("form").submit(function(){
         let msg=$("#text").val();
         if (msg.length===0){
-            $("#btnSubmit").prop("disabled",true);
-        }else{
-            
-            alert(msg);
+            $("#btnSubmit").prop("disabled",true)
         }
+            else{
+                alert(msg);
+                
+            }
+        
     })
-})
 
-$("#btnSubmit").click(function(){
-
-alert(msg);
+ 
+    $("<ul></ul>").appendTo('body');
+  
+$('form').submit(function(e){
+    e.preventDefault();
+    let myMsg= $("#text").val();
+    $('ul').append("<li>"+myMsg+"</li>")
     
 })
+    $("ul").on ("click","li",function(){
+        $(this).css("color",rgb())
+    })
+
+    $("ul").on ("dblclick","li",function(){
+        $(this).remove();
+    })
+
+})
+
+function rgb(){
+    let r=Math.floor(Math.random()*256);
+    let g=Math.floor(Math.random()*256);
+    let b=Math.floor(Math.random()*256);
+    let random ="rgb("+r+", "+g+", "+b+")";
+    return random;
+
+}
+
+
+
+
+    
